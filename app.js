e7f4b2c6bb28fcad58f6eb8f51adbe2818fdf93c
weerapat julagate <weerapat julagate>
2017-01-15 13:31:16 +0700

    ng repeat

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 (function(){
   var app = angular.module('gemStore', []);
-  var gem = { 
-  	name: 'Azurite', 
-  	price: 2.95 
-  };
-  
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
-
+  var gems = [
+	  {
+	  	name: 'Azurite',
+	  	price: 2.95
+	  },
+	  {
+	  	name: 'Bloodstone',
+	  	price: 5.95
+	  },
+	  {
+	  	name: 'Zircon',
+	  	price: 3.95
+	  }
+  ];
 })();
